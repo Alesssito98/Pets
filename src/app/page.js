389116371af4e6bf763f6a041e6b6258756be62f
Alesssito98 <alexgaggero159@gmail.com>
2025-02-17 04:18:41 +0100

    intento de

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,67 @@
 "use client";
-import { getPets } from "./api/pets.js";
+import { getPets, addPet } from "./api/pets.js";
 import { useState, useEffect } from "react";
 
 export default function Home() {
+
   const [pets, setPets] = useState([]);
+  const [nuevoPet, setNewPetName] = useState("");
 
   useEffect(() => {
-    getPets()
-      .then((res) => {
-        console.log("Datos recibidos:", res.data);
-        setPets(res.data);
-      })
-      .catch(console.error);
+
+    getPets().then((res) => {
+
+        const uniquePets = res.data.filter((pet, index, self) => index === self.findIndex((p) => p.id === pet.id));
+        setPets(uniquePets);
+
+      }).catch(console.error);
+
   }, []);
 
-  // Filtrar IDs duplicados
-  const uniquePets = pets.reduce((acc, pet) => {
-    if (!acc.some((p) => p.id === pet.id)) {
-      acc.push(pet);
+  const handleAddPet = async (e) => {
+
+    if (!nuevoPet.trim()) return;
+
+    try {
+
+      const newPet = await addPet({ name: nuevoPet });
+
+      setPets((prevPets) => {
+
+        return [...prevPets, newPet];
+
+      });
+
+      setNewPetName("");
+
+    } catch (error) {
+      console.error("Error al agregar la mascota:", error);
     }
-    return acc;
-  }, []);
+
+  };
 
   return (
-    <div>
-      <h1>Lista de Mascotas</h1>
-      <ul>
-        {uniquePets.map((pet) => (
+    <div  className="contenido">
+      <div>
+        <h1>Lista de Mascotas</h1>
+        <ul>
+          {pets.map((pet) => (
           <li key={pet.id}>{pet.name}</li>
-        ))}
-      </ul>
+          ))}
+        </ul>
+      </div>
+  
+      <div className="formulario">
+        <form onSubmit={handleAddPet}>
+          <input
+            type="text"
+            value={nuevoPet}
+            onChange={(e) => setNewPetName(e.target.value)}
+            placeholder="Nombre de la pet"
+          />
+          <button type="submit">Agregar Pet</button>
+        </form>
+      </div>
     </div>
   );
 }
