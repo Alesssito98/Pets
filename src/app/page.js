1d7cb7670091317446e79fd4ee7455762749a8af
Alesssito98 <alexgaggero159@gmail.com>
2025-02-17 04:46:35 +0100

    me quiero morir

--- src/app/page.js
+++ src/app/page.js
@@ -23,17 +23,18 @@ export default function Home() {
     if (!nuevoPet.trim()) return;
 
     try {
-
-      const newPet = await addPet({ name: nuevoPet });
-
-      setPets((prevPets) => {
-
-        return [...prevPets, newPet];
-
+      const newPet = await addPet({
+        id: Date.now(), // Genera un ID temporal (la API puede sobreescribirlo)
+        name: nuevoPet,
+        status: "available",
+        photoUrls: [],
+        category: { id: 1, name: "General" },
+        tags: [],
       });
-
+  
+      console.log("Nueva mascota agregada:", newPet.data);
+      setPets((prevPets) => [newPet.data, ...prevPets]);
       setNewPetName("");
-
     } catch (error) {
       console.error("Error al agregar la mascota:", error);
     }
@@ -42,7 +43,7 @@ export default function Home() {
 
   return (
     <div  className="contenido">
-      <div>
+      <div className="lista">
         <h1>Lista de Mascotas</h1>
         <ul>
           {pets.map((pet) => (
